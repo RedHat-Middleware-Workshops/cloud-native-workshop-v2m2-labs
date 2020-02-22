39c9b0402ab3fda21d7ea1afa49fed5879f6b7af
danieloh30 <doh@redhat.com>
2020-02-21 20:09:38 -0500

    update delay option

--- monolith/scripts/deploy-inventory.sh
+++ monolith/scripts/deploy-inventory.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USERXX=$1
+DELAY=$2
 
 if [ -z "$USERXX" -o "$USERXX" = "userXX" ]
   then
@@ -30,12 +31,16 @@ oc new-app -e POSTGRESQL_USER=inventory \
 
 oc new-build registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift:1.5 --binary --name=inventory-quarkus -l app=inventory-quarkus
 
+if [ ! -z $DELAY ]
+  then 
+    echo Delay is $DELAY
+    sleep $DELAY
+fi
+
 rm -rf target/binary && mkdir -p target/binary && cp -r target/*runner.jar target/lib target/binary
 
 oc start-build inventory-quarkus --from-dir=target/binary --follow
-
 oc new-app inventory-quarkus -e QUARKUS_PROFILE=prod
-
 oc expose service inventory-quarkus
 
 clear
