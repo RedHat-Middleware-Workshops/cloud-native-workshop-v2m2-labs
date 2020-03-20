1ef9e4a8741f1e401f9506d984f657c0319ce6f6
jamesfalkner <schtool@gmail.com>
2020-03-20 16:45:03 -0400

    Use java:8 imagestream to avoid timeout issues

--- monolith/scripts/deploy-inventory.sh
+++ monolith/scripts/deploy-inventory.sh
@@ -22,7 +22,7 @@ oc new-app -e POSTGRESQL_USER=inventory \
   -e POSTGRESQL_DATABASE=inventory openshift/postgresql:latest \
   --name=inventory-database
 
-oc new-build registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift:1.5 --binary --name=inventory-quarkus -l app=inventory-quarkus
+oc new-build java:8 --binary --name=inventory-quarkus -l app=inventory-quarkus
 
 if [ ! -z $DELAY ]
   then
