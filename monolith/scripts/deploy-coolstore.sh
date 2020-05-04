5ee6698e65089d8ba2d457f1312875bcf01073ab
danieloh30 <doh@redhat.com>
2020-05-04 17:30:56 -0400

    ocp 4.4 update

--- monolith/scripts/deploy-coolstore.sh
+++ monolith/scripts/deploy-coolstore.sh
@@ -28,5 +28,5 @@ oc label dc/coolstore app.openshift.io/runtime=jboss --overwrite && \
 oc label dc/coolstore-postgresql app.kubernetes.io/part-of=coolstore --overwrite && \
 oc label dc/coolstore app.kubernetes.io/part-of=coolstore --overwrite && \
 oc annotate dc/coolstore app.openshift.io/connects-to=coolstore-postgresql --overwrite && \
-oc annotate dc/coolstore app.openshift.io/vcs-uri=https://github.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2m1-labs.git --overwrite && \
+oc annotate dc/coolstore app.openshift.io/vcs-uri=https://github.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2m2-labs.git --overwrite && \
 oc annotate dc/coolstore app.openshift.io/vcs-ref=ocp-4.4 --overwrite
--- monolith/scripts/deploy-inventory.sh
+++ monolith/scripts/deploy-inventory.sh
@@ -30,5 +30,5 @@ oc label dc/inventory-database app.openshift.io/runtime=postgresql --overwrite &
 oc label dc/inventory app.kubernetes.io/part-of=inventory --overwrite && \
 oc label dc/inventory-database app.kubernetes.io/part-of=inventory --overwrite && \
 oc annotate dc/inventory app.openshift.io/connects-to=inventory-database --overwrite && \
-oc annotate dc/inventory app.openshift.io/vcs-uri=https://github.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2m1-labs.git --overwrite && \
+oc annotate dc/inventory app.openshift.io/vcs-uri=https://github.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2m2-labs.git --overwrite && \
 oc annotate dc/inventory app.openshift.io/vcs-ref=ocp-4.4 --overwrite
