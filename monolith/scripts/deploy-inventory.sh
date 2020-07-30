e67319192b5b78865b6177cb7d907c9252fb5e3a
danieloh30 <doh@redhat.com>
2020-07-30 11:26:51 -0400

    fix deployment

--- monolith/scripts/deploy-inventory.sh
+++ monolith/scripts/deploy-inventory.sh
@@ -19,16 +19,16 @@ oc delete dc,deployment,bc,build,svc,route,pod,is --all
 echo "Waiting 30 seconds to finialize deletion of resources..."
 sleep 30
 
-oc new-app -e POSTGRESQL_USER=inventory \
+oc new-app --as-deployment-config -e POSTGRESQL_USER=inventory \
   -e POSTGRESQL_PASSWORD=mysecretpassword \
   -e POSTGRESQL_DATABASE=inventory openshift/postgresql:latest \
   --name=inventory-database
 
 mvn clean package -DskipTests -f $CHE_PROJECTS_ROOT/cloud-native-workshop-v2m2-labs/inventory
 
-oc label deployment/inventory-database app.openshift.io/runtime=postgresql --overwrite && \
+oc label dc/inventory-database app.openshift.io/runtime=postgresql --overwrite && \
 oc label dc/inventory app.kubernetes.io/part-of=inventory --overwrite && \
-oc label deployment/inventory-database app.kubernetes.io/part-of=inventory --overwrite && \
+oc label dc/inventory-database app.kubernetes.io/part-of=inventory --overwrite && \
 oc annotate dc/inventory app.openshift.io/connects-to=inventory-database --overwrite && \
 oc annotate dc/inventory app.openshift.io/vcs-uri=https://github.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2m2-labs.git --overwrite && \
 oc annotate dc/inventory app.openshift.io/vcs-ref=ocp-4.4 --overwrite
