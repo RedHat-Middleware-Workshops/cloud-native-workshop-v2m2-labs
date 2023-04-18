10be5ac9f2e28a1140b7030584b4b262c8c503bb
Daniel Oh <doh@redhat.com>
2023-04-18 11:11:36 +0200

    Update springboot base image

--- monolith/scripts/deploy-catalog.sh
+++ monolith/scripts/deploy-catalog.sh
@@ -29,7 +29,7 @@ oc new-app --as-deployment-config -e POSTGRESQL_USER=catalog \
 
 mvn clean install spring-boot:repackage -DskipTests -f $PROJECT_SOURCE/cloud-native-workshop-v2m2-labs/catalog/
 
-oc new-build registry.access.redhat.com/ubi8/openjdk-17:1.14 --binary --name=catalog-springboot -l app=catalog-springboot
+oc new-build registry.access.redhat.com/ubi8/openjdk-17:latest --binary --name=catalog-springboot -l app=catalog-springboot
 
 if [ ! -z $DELAY ]
   then
